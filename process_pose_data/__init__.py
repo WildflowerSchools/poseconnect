2865a5f95a7ccb5227f2b6ca59fc4eb8cdd18ed4
Theodore Quinn <tcquinn@tcquinn.org>
2020-03-31 12:38:58 -0500

    Rename core submodule to fetch (since that's what it does)

--- process_pose_data/__init__.py
+++ process_pose_data/__init__.py
@@ -1,4 +1,4 @@
-from process_pose_data.core import *
+from process_pose_data.fetch import *
 from process_pose_data.geom_render import *
 from process_pose_data.visualize import *
 from process_pose_data.analyze import *
