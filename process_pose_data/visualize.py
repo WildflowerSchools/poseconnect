9824a73df9bcc1be84c4d66b4a85d8c60d6c0f9a
Theodore Quinn <tcquinn@tcquinn.org>
2020-04-12 12:26:14 -0500

    Delete obsolete functions

--- process_pose_data/visualize.py
+++ process_pose_data/visualize.py
@@ -89,70 +89,6 @@ def track_timeline(
         )
         fig.savefig(path)
 
-def pose_quality_histograms(
-    df,
-    bins=30,
-    show=True,
-    save=False,
-    save_directory='.',
-    filename_suffix='_pose_quality',
-    filename_extension='png',
-    fig_width_inches=10.5,
-    fig_height_inches=8
-):
-    for camera_device_id, group_df in df.groupby('camera_id'):
-        pose_quality_histogram(
-            df=group_df,
-            bins=bins,
-            show=show,
-            save=save,
-            save_directory=save_directory,
-            filename_suffix=filename_suffix,
-            filename_extension=filename_extension,
-            fig_width_inches=fig_width_inches,
-            fig_height_inches=fig_height_inches
-        )
-
-def pose_quality_histogram(
-    df,
-    bins=30,
-    show=True,
-    save=False,
-    save_directory='.',
-    filename_suffix='_pose_quality',
-    filename_extension='png',
-    fig_width_inches=10.5,
-    fig_height_inches=8
-):
-    # Extract camera info
-    camera_id = extract_camera_id(df)
-    # Build plot
-    fig, axes = plt.subplots()
-    plot_object=axes.hist(
-        df['pose_quality'],
-        bins=bins
-    )
-    axes.set_xlabel('Pose quality')
-    axes.set_ylabel('Number of poses')
-    fig.suptitle('{} ({})'.format(
-        camera_id,
-        df['timestamp'].min().isoformat()))
-    fig.set_size_inches(fig_width_inches, fig_height_inches)
-    # Show plot
-    if show:
-        plt.show()
-    # Save plot
-    if save:
-        path = os.path.join(
-            save_directory,
-            '{}{}.{}'.format(
-                slugify.slugify(camera_id),
-                filename_suffix,
-                filename_extension
-            )
-        )
-        fig.savefig(path)
-
 def keypoint_quality_histogram_by_camera(
     df,
     display_camera_name=False,
@@ -671,68 +607,6 @@ def mean_keypoint_quality_pose_quality_scatter(
         )
         fig.savefig(path)
 
-# def pose_keypoint_quality_scatters(
-#     df,
-#     show=True,
-#     save=False,
-#     save_directory='.',
-#     filename_suffix='_pose_keypoint_quality_scatter',
-#     filename_extension='png',
-#     fig_width_inches=10.5,
-#     fig_height_inches=8
-# ):
-#     for camera_device_id, group_df in df.groupby('camera_id'):
-#         pose_keypoint_quality_scatter(
-#             df=group_df,
-#             show=show,
-#             save=save,
-#             save_directory=save_directory,
-#             filename_suffix=filename_suffix,
-#             filename_extension=filename_extension,
-#             fig_width_inches=fig_width_inches,
-#             fig_height_inches=fig_height_inches
-#         )
-#
-# def pose_keypoint_quality_scatter(
-#     df,
-#     show=True,
-#     save=False,
-#     save_directory='.',
-#     filename_suffix='_pose_keypoint_quality_scatter',
-#     filename_extension='png',
-#     fig_width_inches=10.5,
-#     fig_height_inches=8
-# ):
-#     # Extract camera info
-#     camera_id = extract_camera_id(df)
-#     mean_keypoint_quality = df['keypoint_quality_array'].apply(np.nanmean)
-#     # Build plot
-#     fig, axes = plt.subplots()
-#     plot_object=axes.scatter(
-#         df['pose_quality'],
-#         mean_keypoint_quality
-#     )
-#     axes.set_xlabel('Pose quality')
-#     axes.set_ylabel('Mean keypoint quality')
-#     fig.suptitle('{} ({})'.format(
-#         camera_id,
-#         df['timestamp'].min().isoformat()))
-#     fig.set_size_inches(fig_width_inches, fig_height_inches)
-#     # Show plot
-#     if show:
-#         plt.show()
-#     # Save plot
-#     if save:
-#         path = os.path.join(
-#             save_directory,
-#             '{}{}.{}'.format(
-#                 slugify.slugify(camera_id),
-#                 filename_suffix,
-#                 filename_extension
-#             )
-#         )
-#         fig.savefig(path)
-#
 # def pose_track_scores_heatmap(
 #     df,
 #     camera_device_ids,
@@ -813,33 +687,3 @@ def mean_keypoint_quality_pose_quality_scatter(
 #             )
 #         )
 #         fig.savefig(path)
-
-# def extract_camera_id(df):
-#     # Extract camera device ID
-#     camera_device_ids = df['camera_id'].unique().tolist()
-#     if len(camera_device_ids) > 1:
-#         raise ValueError('Data contains more than one camera device ID: {}'.format(
-#             camera_device_ids
-#         ))
-#     camera_device_id = camera_device_ids[0]
-#     return camera_device_id
-
-# def extract_camera_info(df):
-#     # Extract camera device ID
-#     camera_device_ids = df['camera_device_id'].unique().tolist()
-#     if len(camera_device_ids) > 1:
-#         raise ValueError('Data contains more than one camera device ID: {}'.format(
-#             camera_device_ids
-#         ))
-#     camera_device_id = camera_device_ids[0]
-#     # Extract camera name
-#     camera_names = df['camera_name'].unique().tolist()
-#     if len(camera_names) > 1:
-#         raise ValueError('Data contains more than one camera name: {}'.format(
-#             camera_names
-#         ))
-#     camera_name = camera_names[0]
-#     return {
-#         'camera_device_id': camera_device_id,
-#         'camera_name': camera_name
-#     }
