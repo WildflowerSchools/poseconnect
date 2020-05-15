995cec0db684c4b5c7c543877c21c8cde7e463a7
Theodore Quinn <tcquinn@tcquinn.org>
2020-05-14 19:20:25 -0500

    Add ability to pull 2D poses from JSON files

--- process_pose_data/fetch.py
+++ process_pose_data/fetch.py
@@ -4,6 +4,10 @@ import numpy as np
 import tqdm
 import datetime
 import logging
+from uuid import uuid4
+import os
+import re
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -843,3 +847,37 @@ def fetch_extrinsic_calibrations(
     if len(np.unique(space_ids)) > 1:
         raise ValueError('More than one coordinate space found among fetched calibrations')
     return extrinsic_calibrations
+
+def fetch_2d_pose_data_from_local_json(
+    directory_path
+):
+    data = list()
+    for directory_entry in os.listdir(directory_path):
+        if re.match(r'.*\.json', directory_entry):
+            logger.info('Retrieving pose data from {}'.format(directory_entry))
+            with open(os.path.join(directory_path, directory_entry), 'r') as fh:
+                data_this_file = json.load(fh)
+            logger.info('Retrieved {} poses from {}'.format(
+                len(data_this_file),
+                directory_entry
+            ))
+            data.extend(data_this_file)
+    logger.info('Retrieved {} poses overall. Parsing')
+    parsed_data = list()
+    for datum in data:
+        parsed_data.append({
+            'pose_id': uuid4().hex,
+            'timestamp': datum.get('timestamp'),
+            'camera_id': datum.get('camera'),
+            'track_label': datum.get('track_label'),
+            'pose_model_id': datum.get('pose_model'),
+            'keypoint_coordinates': np.asarray([keypoint.get('coordinates') for keypoint in datum.get('keypoints')]),
+            'keypoint_quality': np.asarray([keypoint.get('quality') for keypoint in datum.get('keypoints')]),
+            'pose_quality': datum.get('quality')
+        })
+    df = pd.DataFrame(parsed_data)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    if df['pose_model_id'].nunique() > 1:
+        raise ValueError('Returned poses are associated with multiple pose models')
+    df.set_index('pose_id', inplace=True)
+    return df
