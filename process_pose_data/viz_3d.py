c6a09516a1f3be14ce3c5ab61d09e53042e3aa46
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-14 13:03:32 -0700

    Reformat JSON to group timestamps (attempt 3)

--- process_pose_data/viz_3d.py
+++ process_pose_data/viz_3d.py
@@ -9,7 +9,7 @@ def convert_3d_poses_with_person_info_to_json(
     poses_3d_with_person_info_df = poses_3d_with_person_info_df.copy()
     poses_3d_with_person_info_df.index.name = 'pose_3d_id'
     poses_3d_with_person_info_df.reset_index(inplace=True)
-    poses_3d_with_tracks_df.sort_values('timestamp', inplace=True)
+    poses_3d_with_person_info_df.sort_values('timestamp', inplace=True)
     poses_3d_with_person_info_df['timestamp'] = poses_3d_with_person_info_df['timestamp'].apply(lambda x: x.isoformat())
     poses_3d_with_person_info_df['keypoint_coordinates_3d'] = poses_3d_with_person_info_df['keypoint_coordinates_3d'].apply(lambda x: x.tolist())
     poses_3d_with_person_info_df = poses_3d_with_person_info_df.reindex(columns=[
