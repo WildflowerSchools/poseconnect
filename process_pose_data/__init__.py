d9c11cf1a059bb6d14f065f3678c6bd011d337c6
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-14 12:29:46 -0700

    Implement function for converting identified 3D pose tracks to JSON (attempt 1)

--- process_pose_data/__init__.py
+++ process_pose_data/__init__.py
@@ -3,6 +3,7 @@ from process_pose_data.filter import *
 from process_pose_data.reconstruct import *
 from process_pose_data.visualize import *
 from process_pose_data.overlay import *
+from process_pose_data.viz_3d import *
 from process_pose_data.track_poses import *
 from process_pose_data.identify import *
 from process_pose_data.geom_render import *
