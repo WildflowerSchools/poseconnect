7493d9aa2ebdbe66e85e9a304cb25ef439da8920
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-14 13:01:46 -0700

    Reformat JSON to group timestamps (attempt 2)

--- process_pose_data/viz_3d.py
+++ process_pose_data/viz_3d.py
@@ -9,6 +9,7 @@ def convert_3d_poses_with_person_info_to_json(
     poses_3d_with_person_info_df = poses_3d_with_person_info_df.copy()
     poses_3d_with_person_info_df.index.name = 'pose_3d_id'
     poses_3d_with_person_info_df.reset_index(inplace=True)
+    poses_3d_with_tracks_df.sort_values('timestamp', inplace=True)
     poses_3d_with_person_info_df['timestamp'] = poses_3d_with_person_info_df['timestamp'].apply(lambda x: x.isoformat())
     poses_3d_with_person_info_df['keypoint_coordinates_3d'] = poses_3d_with_person_info_df['keypoint_coordinates_3d'].apply(lambda x: x.tolist())
     poses_3d_with_person_info_df = poses_3d_with_person_info_df.reindex(columns=[
@@ -22,10 +23,7 @@ def convert_3d_poses_with_person_info_to_json(
     ])
     data_dict = OrderedDict()
     for timestamp, timestamp_df in poses_3d_with_person_info_df.groupby('timestamp'):
-        data_dict.append((
-            timestamp,
-            timestamp_df.to_dict(orient='records')
-        ))
+        data_dict[timestamp] = timestamp_df.to_dict(orient='records')
     poses_3d_with_person_info_json = json.dumps(data_dict, indent=2)
     if output_path is not None:
         output_directory = os.path.dirname(output_path)
