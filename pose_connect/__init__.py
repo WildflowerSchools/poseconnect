6e2b40d18011d2569218b4b126a512684e3ca640
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-21 13:38:45 -0700

    Rename pose tracking module

--- pose_connect/__init__.py
+++ pose_connect/__init__.py
@@ -2,7 +2,7 @@ from process_pose_data.filter import *
 from process_pose_data.reconstruct import *
 from process_pose_data.visualize import *
 from process_pose_data.overlay import *
-from process_pose_data.track_poses import *
+from process_pose_data.track import *
 from process_pose_data.identify import *
 
 __version__ = '0.1.0'
