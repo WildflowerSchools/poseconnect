723669285052e9b9ad08357191919b82d9e7a779
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-21 14:10:49 -0700

    Remove obsolete dependencies

--- setup.py
+++ setup.py
@@ -12,11 +12,9 @@ def get_version():
 
 # Dependencies (format is 'PYPI_PACKAGE_NAME[>]=VERSION_NUMBER')
 BASE_DEPENDENCIES = [
-    'wf-honeycomb-io>=0.0.1',
     'wf-smc-kalman>=0.1.0',
     'wf-cv-utils>=3.0.0',
     'wf-video-io>=1.0.0',
-    'wf-geom-render>=0.3.0',
     'pandas>=1.2.2',
     'numpy>=1.20.1',
     'networkx>=2.5',
