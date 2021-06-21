ce6f073a660a426bb30e9f1747332af70352cf79
Theodore Quinn <tcquinn@tcquinn.org>
2021-06-21 13:36:41 -0700

    Remove Wildflower-specific code

--- pose_connect/__init__.py
+++ pose_connect/__init__.py
@@ -1,12 +1,8 @@
-from process_pose_data.local_io import *
 from process_pose_data.filter import *
 from process_pose_data.reconstruct import *
 from process_pose_data.visualize import *
 from process_pose_data.overlay import *
-from process_pose_data.viz_3d import *
 from process_pose_data.track_poses import *
 from process_pose_data.identify import *
-from process_pose_data.geom_render import *
-from process_pose_data.process import *
 
 __version__ = '0.1.0'
