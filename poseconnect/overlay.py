8e426b8cf7d5a3ab4ed421f8d0fe70c8515be747
Theodore Quinn <tcquinn@tcquinn.org>
2021-12-29 19:55:21 -0800

    Remove OpenCV dependency (attempt 1)

--- poseconnect/overlay.py
+++ poseconnect/overlay.py
@@ -7,7 +7,7 @@ import poseconnect.defaults
 import pandas as pd
 import numpy as np
 import cv_utils
-import cv2 as cv
+# import cv2 as cv
 # import ffmpeg
 # import matplotlib.pyplot as plt
 import matplotlib.colors
@@ -614,40 +614,55 @@ def overlay_pose_image(
                 )
     if pd.notna(pose_label):
         pose_label_anchor = np.nanmean(keypoint_coordinates, axis=0)
-        text_box_size, baseline = cv.getTextSize(
-            text=str(pose_label),
-            fontFace=cv.FONT_HERSHEY_PLAIN,
-            fontScale=pose_label_font_scale,
-            thickness=pose_label_line_width
-        )
-        new_image=cv_utils.draw_rectangle(
-            original_image=new_image,
-            coordinates=[
-                [
-                    pose_label_anchor[0] - text_box_size[0]/2,
-                    pose_label_anchor[1] - (text_box_size[1] + baseline)/2
-                ],
-                [
-                    pose_label_anchor[0] + text_box_size[0]/2,
-                    pose_label_anchor[1] + (text_box_size[1] + baseline)/2
-                ]
-            ],
-            line_width=1.5,
-            color=pose_color,
-            fill=True,
-            alpha=pose_label_background_alpha
-        )
-        new_image=cv_utils.draw_text(
+        new_image = cv_utils.draw_text_box(
             original_image=new_image,
             coordinates=pose_label_anchor,
             text=str(pose_label),
             horizontal_alignment='center',
             vertical_alignment='middle',
-            font_face=cv.FONT_HERSHEY_PLAIN,
             font_scale=pose_label_font_scale,
-            line_width=pose_label_line_width,
-            color=pose_label_color
+            text_line_width=pose_label_line_width,
+            text_color=pose_label_color,
+            text_alpha=1.0,
+            box_line_width=1.5,
+            box_color=pose_color,
+            box_fill=True,
+            box_alpha=pose_label_background_alpha
         )
+        # text_box_size, baseline = cv.getTextSize(
+        #     text=str(pose_label),
+        #     fontFace=cv.FONT_HERSHEY_PLAIN,
+        #     fontScale=pose_label_font_scale,
+        #     thickness=pose_label_line_width
+        # )
+        # new_image=cv_utils.draw_rectangle(
+        #     original_image=new_image,
+        #     coordinates=[
+        #         [
+        #             pose_label_anchor[0] - text_box_size[0]/2,
+        #             pose_label_anchor[1] - (text_box_size[1] + baseline)/2
+        #         ],
+        #         [
+        #             pose_label_anchor[0] + text_box_size[0]/2,
+        #             pose_label_anchor[1] + (text_box_size[1] + baseline)/2
+        #         ]
+        #     ],
+        #     line_width=1.5,
+        #     color=pose_color,
+        #     fill=True,
+        #     alpha=pose_label_background_alpha
+        # )
+        # new_image=cv_utils.draw_text(
+        #     original_image=new_image,
+        #     coordinates=pose_label_anchor,
+        #     text=str(pose_label),
+        #     horizontal_alignment='center',
+        #     vertical_alignment='middle',
+        #     font_face=cv.FONT_HERSHEY_PLAIN,
+        #     font_scale=pose_label_font_scale,
+        #     line_width=pose_label_line_width,
+        #     color=pose_label_color
+        # )
     return new_image
 
 def align_timestamps(
